2c990559158075678cd6c9cb087ab0e40b70c92f
Rajdeep Mukherjee <rajdeep@dkr10.cs.ox.ac.uk>
2017-08-04 11:57:00 +0100

    Ultimate progress log

--- UAutomizer-linux/run.sh
+++ UAutomizer-linux/run.sh
@@ -1,2 +1,1 @@
-#runsolver -C 3600 -W 3600 -d 1 -M 32768 
-java -Xmx8G -jar plugins/org.eclipse.equinox.launcher_1.3.100.v20150511-1540.jar -data data/ -tc config/AutomizerReach.xml -s config/svcomp-Reach-32bit-Automizer_Bitvector.epf -i main.c --witnessprinter.generate.witnesses false --traceabstraction.compute.hoare.annotation.of.negated.interpolant.automaton false
+runsolver -C 3600 -W 3600 -d 1 -M 32768 java -Xmx8G -jar plugins/org.eclipse.equinox.launcher_1.3.100.v20150511-1540.jar -data data/ -tc config/AutomizerReach.xml -s config/svcomp-Reach-32bit-Automizer_Bitvector.epf -i main.c --witnessprinter.generate.witnesses false --traceabstraction.compute.hoare.annotation.of.negated.interpolant.automaton false
